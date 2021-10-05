3d1521a5864a9b309f8d208191245a287fe23cc3
Nguyễn Xuân Phú <ngxuanphu21ts444@gmail.com>
2021-10-05 21:43:57 +0700

    1.0

---
+++ themes/default/js/giasuaeba.js
@@ -0,0 +1,20 @@
+/**
+ * @Project GIASU 4.x
+ * @Author TUAN NGUYEN (tuan89dnu@gmail.com)
+ * @Copyright (C) 2016 TUAN NGUYEN. All rights reserved
+ * @License GNU/GPL version 2 or any later version
+ * @Createdate Sat, 11 Jun 2016 16:20:15 GMT
+ */
+
+$(document).ready(function(){
+     $('#form-search-accom').submit(function(e){
+          e.preventDefault();
+          
+          if( $(this).find('[name="submit"]').is(':disabled') ){
+               return;
+          }
+          $(this).find('[name="submit"]').prop('disabled', true);
+          
+          window.location = nv_base_siteurl + 'index.php?' + nv_lang_variable + '=' + nv_lang_data + '&' + nv_name_variable + '=' + nv_module_name + '&' + nv_fc_variable + '=cat-' + $(this).find('[name="cat"]').val() + '/price-' + $(this).find('[name="price"]').val();
+     });
+});
